9a6b6b04054c032094789eafa257ccdbf1eb68dd
Dhiraj Maheswari <dhiraje2002@yahoo.com>
2018-12-07 07:23:29 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$+_cbbn-h9j)*%2*+5$mya1fk05ye16x-dj!%ha%btp#-f)^=='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
